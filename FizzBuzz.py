a56cbe02d5df1c30878f3278cdbbfde8ecab9fc3
Jenny <jenny@cyberfount.com>
2021-01-29 17:55:07 +0000

    fixes 4

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,24 +1,22 @@
 N = 1
+
 for N in range(1, 101):
-    print(N)
 
-DivBy3 = False
-DivBy6 = False
-DivByBoth = False
-
-#smaller number is divisor and larger number is dividend
-
-
-def check_divisibility(dividend, divisor):
-    divisible = False
-    if dividend % divisor == 0:
-        divisible = True
-    print("Divisible by " + str(divisor) + " " + str(divisible))
-    return divisible
+    def check_divisibility(dividend, divisor):
+        divisible = False
+        if dividend % divisor == 0:
+            divisible = True
+        return divisible
 
 
-check_divisibility(9, 3)
-check_divisibility(10, 3)
-check_divisibility(15, 5)
-check_divisibility(16, 5)
+    if (check_divisibility(N, 3)) & (check_divisibility(N, 5)):
+        print("FizzBuzz")
+    elif check_divisibility(N, 3):
+        print("Fizz")
+    elif check_divisibility(N, 5):
+        print("Buzz")
+    else:
+        print(N)
+
+
 
