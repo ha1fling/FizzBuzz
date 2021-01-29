8d0e26afb1f225bd6e901392cffd8cb628d71ecd
Jenny <jenny@cyberfount.com>
2021-01-29 21:47:43 +0000

    refactoring

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -4,17 +4,18 @@ def check_divisibility(dividend, divisor):
 
 def fizz_buzz(n, lower_range, upper_range):
     for n in range(lower_range, upper_range):
-        if check_divisibility(N, 15):
+        if check_divisibility(n, 15):
             print("FizzBuzz")
-        elif check_divisibility(N, 3):
+        elif check_divisibility(n, 3):
             print("Fizz")
-        elif check_divisibility(N, 5):
+        elif check_divisibility(n, 5):
             print("Buzz")
         else:
             print(n)
 
 
 fizz_buzz(1, 1, 101)
+
 
 
 
