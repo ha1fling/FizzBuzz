c283d350a1ec46313dc325cdde723ddd273e5cf0
Jenny <jenny@cyberfount.com>
2021-01-29 23:12:58 +0000

    fixes #7

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,5 +1,19 @@
+def check_divisibility(dividend, divisor)
+    return dividend % divisor == 0
+end
+
 n = 0
 until n == 100
   n += 1
-  puts n
+  if check_divisibility(n,15)
+    puts "FizzBuzz"
+  elsif check_divisibility(n,3)
+    puts "Fizz"
+  elsif check_divisibility(n, 5)
+    puts "Buzz"
+  else
+    puts n
+  end
 end
+
+
