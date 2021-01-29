b48d2f2e02c80fbbe8992bcbb2c6a5839b74f5d4
Jenny <jenny@cyberfount.com>
2021-01-29 23:18:39 +0000

    refactoring

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,19 +1,21 @@
+#!/usr/bin/ruby
 def check_divisibility(dividend, divisor)
     return dividend % divisor == 0
 end
-
-n = 0
-until n == 100
-  n += 1
-  if check_divisibility(n,15)
-    puts "FizzBuzz"
-  elsif check_divisibility(n,3)
-    puts "Fizz"
-  elsif check_divisibility(n, 5)
-    puts "Buzz"
-  else
-    puts n
+def fizz_buzz(n, max)
+  until n == max
+    n += 1
+    if check_divisibility(n,15)
+      puts "FizzBuzz"
+    elsif check_divisibility(n,3)
+      puts "Fizz"
+    elsif check_divisibility(n, 5)
+      puts "Buzz"
+    else
+      puts n
+    end
   end
 end
+fizz_buzz 1, 100
 
 
