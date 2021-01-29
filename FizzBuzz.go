a9e85b5cf448c2b5977b6569eb5b6f479de7076b
Jenny <jenny@cyberfount.com>
2021-01-29 22:33:18 +0000

    FizzBuzz working written in GoLang, fixes #6

--- FizzBuzz.go
+++ FizzBuzz.go
@@ -2,6 +2,18 @@ package main
 import "fmt"
 func main() {
 	for n := 1; n < 101; n++ {
-		fmt.Println(n)
+		if CheckDivisibility(n, 15) {
+			fmt.Println("FizzBuzz")
+		} else if CheckDivisibility(n, 3) {
+			fmt.Println("Fizz")
+		} else if CheckDivisibility(n, 5) {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(n)
+		}
 	}
+}
+
+func CheckDivisibility(dividend int, divisor int) bool {
+	return dividend % divisor == 0
 }
