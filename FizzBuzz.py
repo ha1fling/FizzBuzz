4ccfa75df619290ef601438edc779c49cf217bca
Jenny <jenny@cyberfount.com>
2021-01-29 17:15:51 +0000

    Fixes #3 using a different method, reusing code from issue2 to solve 3 too and to reuse code

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,8 +3,22 @@ for N in range(1, 101):
     print(N)
 
 DivBy3 = False
-Test = 6
-if Test % 3 == 0:
-    DivBy3 = True
-    print(DivBy3)
+DivBy6 = False
+DivByBoth = False
+
+#smaller number is divisor and larger number is dividend
+
+
+def check_divisibility(dividend, divisor):
+    divisible = False
+    if dividend % divisor == 0:
+        divisible = True
+    print("Divisible by " + str(divisor) + " " + str(divisible))
+    return divisible
+
+
+check_divisibility(9, 3)
+check_divisibility(10, 3)
+check_divisibility(15, 5)
+check_divisibility(16, 5)
 
