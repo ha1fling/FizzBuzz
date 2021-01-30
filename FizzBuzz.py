0ef683f4fc890a67b5ce34490d1aee1690a115e4
jashoo <jarret_shook@outlook.com>
2021-01-29 20:45:41 -0800

    Save one divide operation

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,26 +1,42 @@
+#!/usr/bin/env python3
+# Module: FizzBuzz.py
+#
+# Write a program that prints the numbers from 1 to 100. But for multiples of 
+# three print “Fizz” instead of the number and for the multiples of five print 
+# “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz"
+
 def check_divisibility(dividend, divisor):
+    """ Checks whether a dividend is divsible by the divsor passed
+
+    Args:
+        dividend (int)
+        divisor (int)
+
+    Returns:
+        bool
+    """
     return dividend % divisor == 0
 
+def fizz_buzz(lower_range, upper_range):
+    """ FizzBuzz implementation
 
-def fizz_buzz(n, lower_range, upper_range):
+    Args:
+        lower_range (int)
+        upper_range (int)
+    """
     for n in range(lower_range, upper_range):
-        if check_divisibility(n, 15):
-            print("FizzBuzz")
-        elif check_divisibility(n, 3):
-            print("Fizz")
-        elif check_divisibility(n, 5):
-            print("Buzz")
+        divisable_by_three = check_divisibility(n, 3)
+        divisable_by_five = check_divisibility(n, 5)
+
+        if divisable_by_three and divisable_by_five:
+            yield "FizzBuzz"
+        elif divisable_by_three:
+            yield "Fizz"
+        elif divisable_by_five:
+            yield "Buzz"
         else:
-            print(n)
+            yield n
 
-
-fizz_buzz(1, 1, 101)
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    for item in fizz_buzz(1, 101):
+        print (item)
